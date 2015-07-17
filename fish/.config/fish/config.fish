a9126c192fd09dd58f0f85670a5e8912ff993ff5
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2015-07-17 22:56:34 +1000

    Update fish config to match zsh's theme (pure).

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,25 +1,21 @@
 # Path to your oh-my-fish.
-# set fish_path $HOME/.oh-my-fish
+set fish_path $HOME/.oh-my-fish
 
-# Theme
-# set fish_theme agnoster
+# Use minimal theme (Similar to zsh's pure)
+set fish_theme clearance
 
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Enable plugins by adding their name separated by a space to the line below.
-# set fish_plugins theme
-
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+set fish_plugins archlinux theme python
 
 # Load oh-my-fish configuration.
-# . $fish_path/oh-my-fish.fish
+source $fish_path/oh-my-fish.fish
 
-
-function fish_prompt
-  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh left
-end
-
-function fish_right_prompt
-  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh right
-end
+# function fish_prompt
+#   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh left
+# end
+#
+# function fish_right_prompt
+#   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh right
+# end
