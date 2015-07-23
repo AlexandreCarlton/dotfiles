0ae7eaa02945eb86f50963b7518f7663e4a4b68e
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2015-07-23 15:28:32 +1000

    Minor change to volume script for conky

--- binaries/.bin/volume.sh
+++ binaries/.bin/volume.sh
@@ -8,16 +8,17 @@ LOW_ICON="\uE04E"
 MEDIUM_ICON="\uE050"
 HIGH_ICON="\uE05D"
 
-if [[ ! -z $is_muted ]]; then
+if [ ! -z $is_muted ]; then
     icon=$MUTE_ICON
 else
-    if [[ $volume -le 33 ]]; then
+    if [ $volume -le 33 ]; then
         icon=$LOW_ICON
-    elif [[ $volume -le 66 ]]; then
+    elif [ $volume -le 66 ]; then
         icon=$MEDIUM_ICON
     else
         icon=$HIGH_ICON
     fi
 fi
 
-echo -e "$icon $volume"
+#echo -e "$icon $volume"
+echo $volume
