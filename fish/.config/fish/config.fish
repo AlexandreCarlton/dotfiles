c0694e590872deb71092488f7df9298705e2b689
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2016-04-03 23:02:55 +1000

    [fish] Add fisherman
    Trying out fisherman with the fish shell; hopefully these files will be
    enough to reproduce the settings on another machine.
    If they aren't, look at the Makefile in ~/.local/share/fisherman to see
    what needs to be made.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,3 @@
-# Path to your oh-my-fish.
-# set fish_path $HOME/.oh-my-fish
-
-# Use minimal theme (Similar to zsh's pure)
-set fish_theme clearance
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-# set fish_plugins archlinux theme python
-
-# Load oh-my-fish configuration.
-#source $fish_path/oh-my-fish.fish
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
