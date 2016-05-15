63a59c404b967ff4dab81a4847ef7f00b2c68182
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2016-05-15 21:54:18 +1000

    Fix bug when installing fisherman
    Now we check that the file exists before trying to use it to update
    plugins.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
+# TODO: Find a way to actually check the function exists, not the file itself.
 if test ! -s $HOME/.config/fish/functions/fisher.fish
   curl -fLo $HOME/.config/fish/functions/fisher.fish --create-dirs \
     https://raw.githubusercontent.com/fisherman/fisherman/master/fisher.fish
-  if test ! -s $HOME/.config/fish/fishfile
+  if test -s $HOME/.config/fish/fishfile
     fisher update
   end
 end
