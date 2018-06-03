153b3ad0cee48f40a4e1bf7e84adb094ec79bde1
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2018-06-03 18:28:57 +1000

    Fix pixel_lock.sh not activating

--- binaries/.local/bin/pixel_lock.sh
+++ binaries/.local/bin/pixel_lock.sh
@@ -3,16 +3,16 @@
 # Takes a screenshot, pixelates it and uses it as an image for i3lock.
 
 # Dependencies:
-# - GraphicsMagick (faster than ImageMagick
+# - GraphicsMagick (faster than ImageMagick)
 # - i3lock
 # - scrot
 
 
 screenshot=$(mktemp --tmpdir XXXX.png)
+trap 'rm $screenshot' EXIT
+
 scrot "${screenshot}"
 gm mogrify -scale 10% -scale 1005% "${screenshot}"
 i3lock --ignore-empty-password \
-       --no-unlock-indicator \
        --nofork \
        --image="$screenshot"
-rm "$screenshot"
