41e1b315517c4e0a627d893942e1ac4a5995490f
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-07-11 05:08:04 +1000

    Add eager tmux attaching in fish

---
+++ fish/.config/fish/conf.d/00-tmux-attach.fish
@@ -0,0 +1,7 @@
+# Attach to existing tmux session if not already in one, or create one if one does not exist.
+# doing this early prevents the original shell from executing everything
+# beneath until the user detaches.
+if set -q TMUX and $TERM != linux
+	# if the session 'alexandre' already exists, tmux attaches to it; otherwise, it creates a new one.
+  tmux new-session -A -s alexandre
+end
