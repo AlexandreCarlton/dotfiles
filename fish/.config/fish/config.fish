93955bff99cd9b3fad42f06108c24d8c4458306b
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-07-15 03:27:57 +1000

    Set pure prompt default to green
    This makes it more obvious when we get a non-0 exit code.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,8 @@ if status --is-interactive
   # brblack is invisible in solarized (dark), so we set the pure theme's colors differently.
   set -g pure_color_git_branch (set_color normal)
   set -g pure_color_git_dirty (set_color normal)
+  # Use green for success; magenta is too similar to red.
+  set -g pure_color_prompt_on_success (set_color green)
 
   for aliases in $HOME/.config/fish/aliases/*
     source $aliases
