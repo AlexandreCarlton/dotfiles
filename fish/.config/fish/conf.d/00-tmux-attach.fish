3b24733db8e0db6a0543a3a973768034af8392dc
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-09-07 12:42:02 +1000

    Don't attach to tmux in Jetbrains terminal

--- fish/.config/fish/conf.d/00-tmux-attach.fish
+++ fish/.config/fish/conf.d/00-tmux-attach.fish
@@ -1,7 +1,9 @@
 # Attach to existing tmux session if not already in one, or create one if one does not exist.
 # doing this early prevents the original shell from executing everything
 # beneath until the user detaches.
-if ! set -q TMUX; and test "$TERM" != linux
+if ! set -q TMUX;
+  and test "$TERM" != linux;
+  and test "$TERMINAL_EMULATOR" != 'Jetbrains-JediTerm'
 	# if the session 'alexandre' already exists, tmux attaches to it; otherwise, it creates a new one.
   tmux new-session -A -s alexandre
 end
