085d609d500e33b20932b38071e67b269a0caa2c
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2020-08-29 19:12:42 +1000

    Revert Intellij terminal detection

--- fish/.config/fish/conf.d/00-tmux-attach.fish
+++ fish/.config/fish/conf.d/00-tmux-attach.fish
@@ -3,7 +3,7 @@
 # beneath until the user detaches.
 if ! set -q TMUX;
   and test "$TERM" != linux;
-  and test "$TERMINAL_EMULATOR" != 'Jetbrains-JediTerm'
+  and test "$TERMINAL_EMULATOR" != 'JetBrains-JediTerm'
 	# if the session 'alexandre' already exists, tmux attaches to it; otherwise, it creates a new one.
   tmux new-session -A -s alexandre
 end
