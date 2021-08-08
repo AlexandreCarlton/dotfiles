f5e9151c74d5501ad119f073020d2e307f7fd514
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2021-08-08 17:28:28 +1000

    Loading certain vim plugins on insert

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -3,7 +3,8 @@
 vim.cmd [[ runtime! plugin/python_setup.vim ]]
 
 -- Install via git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
--- Run :PackerSync to update
+-- Run :PackerCompiler after _any_ change.
+-- Run :PackerSync to update.
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
@@ -71,60 +72,63 @@ require('packer').startup(function()
   -- Completion {{{
 
   -- Autocompleteion for quotes, parentheses, etc.
-  use 'raimondi/delimitMate' --- {{{
+  use { 'raimondi/delimitMate', opt = true, event = 'InsertEnter' } --- {{{
   -- Turn on expansion of <Space>.
   vim.g.delimitMate_expand_cr = 1
   --- }}}
 
   -- End certain structures automatically
-  use 'tpope/vim-endwise'
+  use { 'tpope/vim-endwise', opt = true, event = 'InsertEnter' }
 
   -- LSP support
-  use 'neovim/nvim-lspconfig' -- {{{
-  require'lspconfig'.efm.setup {
-    filetypes = {"sh", "Dockerfile", "yaml"}
-  }
-  require'lspconfig'.pylsp.setup {
-    cmd = { "pyls" }
-  }
-  -- }}}
+  use { 'neovim/nvim-lspconfig',  -- {{{
+    config = function()
+      require'lspconfig'.efm.setup { filetypes = {"sh", "Dockerfile", "yaml"} }
+      require'lspconfig'.pylsp.setup { cmd = { "pyls" } }
+    end
+  } -- }}}
 
   -- Autocompletion (recommended by nvim-lspconfig)
-  use 'hrsh7th/nvim-compe' --- {{{
-  vim.o.completeopt = "menuone,noselect"
-  require'compe'.setup {
-    source = {
-      path = true,
-      buffer = true,
-      nvim_lsp = true,
-      nvim_lua = true,
-    }
-  }
-  -- (S-)Tab to cycle through suggestions
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
-  local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-  end
-  _G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-      return t "<C-n>"
-    elseif check_back_space() then
-      return t "<Tab>"
-    else
-      return vim.fn['compe#complete']()
+  use { 'hrsh7th/nvim-compe', --- {{{
+    opt = true,
+    event = 'InsertEnter',
+    requires = {'neovim/nvim-lspconfig'},
+    config = function()
+      vim.o.completeopt = "menuone,noselect"
+      require'compe'.setup {
+        source = {
+          path = true,
+          buffer = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+        }
+      }
+      -- (S-)Tab to cycle through suggestions
+      local t = function(str)
+        return vim.api.nvim_replace_termcodes(str, true, true, true)
+      end
+      local check_back_space = function()
+        local col = vim.fn.col('.') - 1
+        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+      end
+      _G.tab_complete = function()
+        if vim.fn.pumvisible() == 1 then
+          return t "<C-n>"
+        elseif check_back_space() then
+          return t "<Tab>"
+        else
+          return vim.fn['compe#complete']()
+        end
+      end
+      _G.s_tab_complete = function()
+        return vim.fn.pumvisible() == 1 and t "<C-p>" or t "<S-Tab>"
+      end
+      vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
     end
-  end
-  _G.s_tab_complete = function()
-    return vim.fn.pumvisible() == 1 and t "<C-p>" or t "<S-Tab>"
-  end
-  vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-  vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-  -- }}}
+  } -- }}}
 
   -- }}}
 
@@ -156,8 +160,12 @@ require('packer').startup(function()
 
   -- Show disagnostics in loclist
   use { 'folke/trouble.nvim', -- {{{
-    requires = { 'kyazdani42/nvim-web-devicons'},
-    config = function() require'trouble'.setup {} end
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function()
+      require'trouble'.setup {
+        icons = false
+      }
+    end
   } -- }}}
 
   -- Create missing highlight LSP diagnostics groups for colourschemes that don't have them.
@@ -169,10 +177,11 @@ require('packer').startup(function()
   -- Display git diff in the gutter
   -- ]c / [c to jump between hunks
   use { 'lewis6991/gitsigns.nvim', -- {{{
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  require'gitsigns'.setup {}
-  -- }}}
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require'gitsigns'.setup {}
+    end
+  } -- }}}
 
   -- Intelligent number toggling
   use 'myusuf3/numbers.vim' -- {{{
@@ -230,6 +239,7 @@ require('packer').startup(function()
   vim.g.EditorConfig_exclude_patterns = {'fugitive://.*', 'tarfile::.*'}
   vim.g.EditorConfig_max_line_indictor = 'line'
   vim.g.EditorConfig_core_mode = 'external_command'
+  vim.g.EditorConfig_exec_path = '/usr/bin/editorconfig'
   -- }}}
 
   -- }}}
@@ -269,10 +279,8 @@ require('packer').startup(function()
   -- Run things in tmux pane
   use { 'tpope/vim-dispatch', opt = true, cmd = { 'Make', 'Dispatch', 'Start' } }
 
-  use { 'mhinz/vim-grepper', opt = true, cmd = { 'Grepper' } }
-  -- There is a bug with neovim that prevents rg from working correctly - otherwise we'd use rg by default.
-  -- vim.g.grepper = { tools = {'rg'} }
-
+  -- Profile vim startup if it gets slow.
+  use { 'tweekmonster/startuptime.vim', opt = true, cmd = {'StartupTime'} }
   -- }}}
 
   -- Navigation {{{
@@ -281,29 +289,32 @@ require('packer').startup(function()
     requires = {
       { 'nvim-lua/popup.nvim' },
       { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-    }
-  }
-  require'telescope'.setup {
-    defaults = {
-      mappings = {
-        -- Ctrl-T will open the search results in trouble.
-        i = { ["<c-t>"] = require'trouble.providers.telescope'.open_with_trouble },
-        n = { ["<c-t>"] = require'trouble.providers.telescope'.open_with_trouble },
-      }
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      { 'folke/trouble.nvim' }
     },
-    extensions = {
-      -- Use telescope's C implementation of fzf
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = false,
-        override_file_sorter = true,
-        case_mode = "smart_case",
+    config = function()
+      require'telescope'.setup {
+        defaults = {
+          mappings = {
+            -- Ctrl-T will open the search results in trouble.
+            i = { ["<c-t>"] = require'trouble.providers.telescope'.open_with_trouble },
+            n = { ["<c-t>"] = require'trouble.providers.telescope'.open_with_trouble },
+          }
+        },
+        extensions = {
+          -- Use telescope's C implementation of fzf
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = false,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          }
+        }
       }
-    }
+      -- Requried to load fzf and have it work with telescope.
+      require'telescope'.load_extension'fzf'
+    end
   }
-  -- Requried to load fzf and have it work with telescope.
-  require'telescope'.load_extension'fzf'
   vim.cmd [[
     nnoremap <leader>ff <cmd>Telescope find_files<cr>
     nnoremap <leader>fg <cmd>Telescope live_grep<cr>
