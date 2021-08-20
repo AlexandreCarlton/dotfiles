f3a186f5edf98c5d87997f70b585570a5bb6259e
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2021-08-20 10:53:25 +1000

    Use tide prompt
    This is fully asynchronous. The only thing I'm not wild about is that
    you can't easily export settings into dotfiles.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,13 +3,6 @@
 # We wrap stty within `--is-interactive` as fisher will hang otherwise.
 if status --is-interactive
 
-  # Tweak the pure theme (requires fisher - https://git.io/fisher)
-  # brblack is invisible in solarized (dark), so we set the pure theme's colors differently.
-  set -g pure_color_git_branch (set_color normal)
-  set -g pure_color_git_dirty (set_color normal)
-  # Use green for success; magenta is too similar to red.
-  set -g pure_color_prompt_on_success (set_color green)
-
   for aliases in $HOME/.config/fish/aliases/*
     source $aliases
   end
