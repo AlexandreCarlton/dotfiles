850fa464ea00a7c09f88339677ecbcae45093711
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2024-01-05 08:27:22 +1100

    Auto-start sway if it exists from fish
    sway seems to be a little allergic to being started from systemd.
    Without any (almost deliberate?) official support, we just run it from a
    tty in our config.fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,13 @@
 # We wrap stty within `--is-interactive` as fisher will hang otherwise.
 if status --is-interactive
 
+  # Autostart sway if it is present - it doesn't like  being run via systemd
+  # (Should probably be in a conf.d/sway-autostart.fish file)
+  if type -q sway; and [ (tty) = '/dev/tty1' ]; and not set -q DISPLAY; and not set -q TMUX
+    set -x WLR_NO_HARDWARE_CURSORS 1
+    systemd-cat --identifier=sway sway --unsupported-gpu --verbose
+  end
+
   for aliases in $HOME/.config/fish/aliases/*
     source $aliases
   end
